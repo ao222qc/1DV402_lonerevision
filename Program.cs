f5b59f4f1bbdcfa9c8f7b44ac2be1176f280b4a9
ao222qc <ao222qc@student.lnu.se>
2014-09-19 12:32:27 +0200

    Lab 3 up and running

--- Program.cs
+++ Program.cs
@@ -9,45 +9,57 @@ namespace lab3lonerevision
     {
         static void Main(string[] args)
         {
-            //Läs in antal index i arrayen (löner i arrayen)
-            int numberOfSalaries = ReadInt("Enter total number of salaries: ");
-            
-            //Läs in de olika lönerna med hjälp av array
-            ProcessSalaries(numberOfSalaries);
-
-            //Skriv ut.
+            do
+            {
+                int numberOfSalaries = ReadInt("Enter total number of salaries: ");
+                Console.Write("\n");
+                if (numberOfSalaries >= 2)
+                {
+                    ProcessSalaries(numberOfSalaries);
+                }
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("Enter at least two salaries to perform a calculation!");
+                    Console.ResetColor();
+                    Console.WriteLine("");
+                    Console.BackgroundColor = ConsoleColor.DarkGreen;
+                    Console.WriteLine("Press any key for new calculation - ESC closes application.");
+                    Console.ResetColor();
+                }
+            }
+            while (Console.ReadKey(true).Key != ConsoleKey.Escape);
         }
 
         static void ProcessSalaries(int count)
         {
             int[] salaries = new int[count];
-
+            int[] salariesUnsorted = new int[count];
+            int number = 0;
             for (int i = 0; i < count; i++)
             {
-                Console.Write("Enter the salary number {0}:", i);
+                number += 1;
+                Console.Write("Enter the salary number {0}:", number);
                 salaries[i] = int.Parse(Console.ReadLine());
             }
 
+            Array.Copy(salaries, salariesUnsorted, count);
             Array.Sort(salaries);
 
             if (count % 2 != 0)
             {
-                int median;
-                median = salaries.Length / 2;
+                int median = salaries.Length / 2;
                 Console.WriteLine("---------------------------");
-                Console.Write("Median salary  : {0,10:c0}", salaries[median]);
-                Console.WriteLine("\n");
+                Console.WriteLine("Median salary  : {0,10:c0}", salaries[median]);
+                
                 //mittersta värdet = medianlön      
             }
 
             else
             {
-                int median;
-                int value1;
-                int value2;
-                value1 = salaries.Length / 2;
-                value2 = value1 - 1;
-                median = salaries[value1] + salaries[value2];
+                int value1 = salaries.Length / 2;
+                int value2 = value1 - 1;
+                int median = salaries[value1] + salaries[value2];
                 Console.WriteLine("---------------------------");
                 Console.WriteLine("Median salary  : {0,10:c0}", median / 2);
                 //Två mittersta värdena/2 = medianlön
@@ -55,10 +67,10 @@ namespace lab3lonerevision
             //averagepayuträkning
 
             int totalPay = 0;
+
             for (int j = 0; j < count; j++)
             {
                 totalPay += salaries[j];
-
             }
             Console.WriteLine("Average salary : {0,10:c0}", totalPay / count);
 
@@ -67,14 +79,39 @@ namespace lab3lonerevision
             Console.WriteLine("Salary spread  : {0,10:c0}", salarySpread);
             Console.WriteLine("---------------------------");
 
-        }
+            for (int k = 0; k < count; k++)
+            {
+                if (k % 3 == 0)
+                {
+                    Console.WriteLine("");
+                }
 
+                Console.Write("{0,9}", salariesUnsorted[k]);
+            }
+            Console.WriteLine("\n");
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Press any key for a new calculation - ESC closes the application.");
+            Console.ResetColor();
+        }
 
         static int ReadInt(string prompt)
         {
-            Console.Write(prompt);
-            int numberOfSalaries = int.Parse(Console.ReadLine());
-            return numberOfSalaries;
+            while (true)
+            {
+                try
+                {
+                    Console.Write(prompt);
+                    int numberOfSalaries = int.Parse(Console.ReadLine());
+                    return numberOfSalaries;
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("Please enter a valid format!");
+                }
+            }
+
         }
 
     }
